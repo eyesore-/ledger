59d0e324993c618ea408ede6e6a5a5203a3ecd0d
Joel <joelbmolina@gmail.com>
2017-11-28 15:44:11 -0700

    forEach and map method ready for implementation

---
+++ src/forEach.js
@@ -0,0 +1,21 @@
+/*
+forEach() method executes a provided function once to each
+array element
+
+*Syntax:
+  arr.forEach(function(currentValue, index, array){
+  // code block to call on each array element
+})
+
+currentValue: the current value being processed in the array
+index (optional): the index of the current value being processed
+array (optional): the input array that map was called upon
+
+* Since we are not going to hijack the native .forEach()
+method we will be passing the input array as the
+first argument
+*/
+
+const forEach = function(arr, callback) {
+  // implement forEach here
+}
---
+++ src/map.js
@@ -0,0 +1,24 @@
+/*
+The map() method creates a new array with the results
+of calling a callback function on every value of the
+calling array
+
+*Syntax:
+  var newArr = arr.map(function callback(currentValue, index, array){
+    // Return element for newArr
+  })
+
+Map callback parameters are - (currentValue, index, array)
+
+currentValue: the current value being processed in the array
+index (optional): the index of the current value being processed
+array (optional): the input array that map was called upon
+
+* Since we are not going to hijack the native .map()
+method we will be passing the input array as the
+first argument
+*/
+
+const map = function(arr, callback) {
+  // implement map here
+}
